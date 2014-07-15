5a7c9be8d1db7ae4ce497395c0faa373668f6081
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2014-07-15 10:20:28 -0400

    Initial parser code

--- public/rails_json_errors.js
+++ public/rails_json_errors.js
@@ -1,1 +1,13 @@
 window.RJE = window.RJW || {};
+
+window.RJE.displayJsonErrors = function(rawJson) {
+  var json, message;
+  message = [];
+  json = $.parseJSON(rawJson);
+  _.each(json.errors, function(val, key) {
+    return _.each(val, function(elm) {
+      return message.push(key + ": " + elm);
+    });
+  });
+  return alert(message.join("\n"));
+};
--- src/rails_json_errors.coffee
+++ src/rails_json_errors.coffee
@@ -1,2 +1,10 @@
 window.RJE = window.RJW || {}
 
+window.RJE.displayJsonErrors = (rawJson) ->
+  message = []
+  json = $.parseJSON(rawJson)
+  _.each json.errors, (val, key) ->
+    _.each val, (elm) ->
+      message.push(key+": "+elm)
+
+  alert message.join("\n")
